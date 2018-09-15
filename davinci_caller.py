3507d74241fe0522be07cebae01200d1fb1776d0
chaitanyavp <chaitanya1998@gmail.com>
2018-09-15 16:42:53 -0400

    firebase support

--- davinci_caller.py
+++ davinci_caller.py
@@ -1,16 +1,31 @@
 import requests
+from firebase import firebase
 
 
-api_file = open("api.key", "r")
-api_key = api_file.readline().rstrip("\n")
-api_file.close()
+def iter_customer_transactions():
+    api_file = open("api.key", "r")
+    api_key = api_file.readline().rstrip("\n")
+    api_file.close()
 
-customer_file = open("sample_customer", "r")
-customer_key = customer_file.readline().rstrip("\n")
-customer_file.close()
+    customer_file = open("sample_customer", "r")
+    customer_key = customer_file.readline().rstrip("\n")
+    customer_file.close()
 
-response = requests.get("https://api.td-davinci.com/api/customers/" + customer_key + "/transactions", headers ={'Authorization': api_key})
-response_data = response.json()
+    response = requests.get("https://api.td-davinci.com/api/customers/" + customer_key + "/transactions", headers={'Authorization': api_key})
+    response_data = response.json()
 
-print(response_data["statusCode"])
-print(response_data["result"])
+    if response_data["statusCode"] == 200:
+        return response_data["result"]
+    else:
+        return None
+
+
+def read_firebase():
+    fb = firebase.FirebaseApplication(
+        'https://elixr-37b8a.firebaseio.com')
+    result = fb.get('/users', None)
+    print(result)
+
+
+if __name__ == "__main__":
+    read_firebase()
