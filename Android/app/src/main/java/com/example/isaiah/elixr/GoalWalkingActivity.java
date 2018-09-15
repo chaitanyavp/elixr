77d4881b1acaf723a6ce941557ca2017845da9ca
Isaiah <isaiah@v1020-wn-35-62.campus-dynamic.uwaterloo.ca>
2018-09-15 18:45:11 -0400

    added two new screens, implementing the pedometer now, TODO: work on firebase integration

---
+++ Android/app/src/main/java/com/example/isaiah/elixr/GoalWalkingActivity.java
@@ -0,0 +1,16 @@
+package com.example.isaiah.elixr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoalWalkingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goal_walking);
+
+
+        
+    }
+}
