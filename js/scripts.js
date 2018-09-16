d025e41edd7245a6e101d7557d320ddefdaff482
chaitanyavp <chaitanya1998@gmail.com>
2018-09-15 20:30:55 -0400

    fix for communication errors between js and flask

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-var button = document.getElementbyId("testButton");
+var button = document.querySelector("#testButton");
 button.onclick = functionTest;
 
 function functionTest() {
     var request = new XMLHttpRequest();
     request.open('GET', 'http://127.0.0.1:5000/test', true);
-    request.responseText = "string"
+    request.responseText = "text"
 
     request.onreadystatechange = function () {
         if (this.readyState === 4) {
@@ -14,5 +14,5 @@ function functionTest() {
         }
     };
 
-    request.send();
+    request.send(null);
 }
