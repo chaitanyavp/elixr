cce38a2cc604d58a22988280c1edc64a837b5497
ntn <neethan.bala@gmail.com>
2018-09-16 01:55:48 -0400

    Added nutrition data visualizers, and intial outline for goal creation

---
+++ js/other.js
@@ -0,0 +1,7 @@
+var button = document.querySelector("#add-new");
+button.onclick = functionTest;
+
+function functionTest() {
+let field = document.querySelector("#input-one");
+console.log(field.innerText)
+}
