1c23389ce9bfd91d62d3c3692177137e29edfece
ntn <neethan.bala@gmail.com>
2018-09-15 20:13:49 -0400

    Minor changes to test get request

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,15 @@ button.onclick = functionTest;
 function functionTest() {
     var request = new XMLHttpRequest();
     request.open('GET', 'http://127.0.0.1:5000/test', true);
+    request.responseText = "string"
 
-    request.onload = function () {
-        console.log(request.responseText)
+    request.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            console.log('Status:', this.status);
+            console.log('Headers:', this.getAllResponseHeaders());
+            console.log('Body:', this.response);
+        }
     };
 
-    request.send(null);
+    request.send();
 }
